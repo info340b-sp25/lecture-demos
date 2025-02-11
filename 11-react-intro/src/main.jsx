cfc1f8c6e0380911a32fba9d089193b2dbe48ea0
Kyle <kmthayer@uw.edu>
2025-02-11 14:32:48 -0800

    break out into subcomponents

--- 11-react-intro/src/main.jsx
+++ 11-react-intro/src/main.jsx
@@ -4,22 +4,32 @@ import './index.css';
 
 const my_name = "Kyle"
 
+function HelloMessage(props) {
+    return (
+        <h1 id='hello' className='myClass'>'Hello {my_name}!'</h1>
+    )
+}
+
+function App(props) {
+    return (
+        <div>
+            <header>
+                <HelloMessage />
+            </header>
+            <footer>
+                <p>Goodby {my_name}. This page is &copy; INFO 340</p>
+            </footer>
+        </div>
+    )
+}
+
 //React - element to show
-const msgElem = (
-    <div>
-        <header>
-            <h1 id='hello' className='myClass'>'Hello {my_name}!'</h1>
-        </header>
-        <footer>
-            <p>Goodby {my_name}. This page is &copy; INFO 340</p>
-        </footer>
-    </div>
+const appElem = (
+    <App />
 );
-
-
 
 //Create a "React root" out of the `#root` elemment
 //then render the React element at that root
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(msgElem)
+root.render(appElem)
   
