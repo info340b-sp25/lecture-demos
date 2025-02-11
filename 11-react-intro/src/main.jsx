48a56785358370705d5238c2a9887e08a780a2c1
Kyle <kmthayer@uw.edu>
2025-02-11 14:17:31 -0800

    start creating dom in react

--- 11-react-intro/src/main.jsx
+++ 11-react-intro/src/main.jsx
@@ -2,3 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const my_name = "Kyle"
+
+//React - element to show
+const msgElem = (
+    <div>
+        <header>
+            <h1 id='hello' className='myClass'>'Hello {my_name}!'</h1>
+        </header>
+        <footer>
+            <p>Goodby {my_name}. This page is &copy; INFO 340</p>
+        </footer>
+    </div>
+);
+
+
+
+//Create a "React root" out of the `#root` elemment
+//then render the React element at that root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(msgElem)
+  
