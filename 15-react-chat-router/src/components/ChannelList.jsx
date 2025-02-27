97e00cad6f909d4e0a2cf5c2b16021911f680227
Kyle <kmthayer@uw.edu>
2025-02-27 15:23:25 -0800

    add a tracking of favorited channels

--- 15-react-chat-router/src/components/ChannelList.jsx
+++ 15-react-chat-router/src/components/ChannelList.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {Link} from 'react-router-dom';
 
 export function ChannelList(props) {
   const { channelNames, currentChannel } = props;
+
+  const [faveChans, setFaveChans ] = useState([])
+  console.log("faveChans", faveChans)
+
+  const handleFaveClick = (event) => {
+    const buttonChanName = event.target.getAttribute("data-channel-name")
+    if(faveChans.includes(buttonChanName)){
+      const index = faveChans.indexOf(buttonChanName);
+      if (index > -1) { // only splice array when item is found
+        faveChans.splice(index, 1); // 2nd parameter means remove one item only
+      }
+    }else {
+      console.log("adding channel", buttonChanName)
+      faveChans.push(buttonChanName)
+    }
+    const newFaveChans = [...faveChans];
+    setFaveChans(newFaveChans);
+  }
 
   //render the links
   const liArray = channelNames.map((channelNameString) => {
@@ -17,7 +35,12 @@ export function ChannelList(props) {
           to={"/chat/"+channelNameString}
         >
           {channelNameString}
-        </Link>
+        </Link> 
+        <button 
+          Style={faveChans.includes(channelNameString)? "color:red" : "color:gray"}
+          onClick={handleFaveClick}
+          data-channel-name={channelNameString}
+          >*</button>
       </div>
     );
   })
